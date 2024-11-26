3b9579047bac3b0277ef02ef1cc8de7a54c35e08
tmrwrainagain <alxmrn09@gmail.com>
2024-11-27 04:10:30 +1000

    Добавьте файлы проекта.

---
+++ WpfApp5/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace ImageChangeApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void checkBox_CheckedChanged(object sender, RoutedEventArgs e)
+        {
+            if (checkBox.IsChecked == true)
+            {
+                // Изменение картинки при отметке флажка
+                image.Source = new BitmapImage(new Uri("Images/image1.jpeg"));
+            }
+            else
+            {
+                // Возврат к начальной картинке при снятии флажка
+                image.Source = new BitmapImage(new Uri("Images/image2.jpeg"));
+            }
+        }
+    }
+}
